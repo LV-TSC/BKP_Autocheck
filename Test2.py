0c78d2ea0cd46deb54923d4a62542de29dd05aa0
LizandroV <lizandrovivanco16@gmail.com>
2022-10-28 17:09:47 -0500

    Se esta volviendo a actualizar

--- Test2.py
+++ Test2.py
@@ -1,20 +1,143 @@
-
-import logging
 import os
+import glob
+import pyodbc
+import time
+from datetime import date
+from datetime import timedelta
+import subprocess
+from pathlib import Path
+
+CREATE_NO_WINDOW = 0x08000000
+subprocess.call("""net use S: /del /y""", creationflags=CREATE_NO_WINDOW)
+subprocess.call("""net use S: "\\172.16.88.10\bk_ftp" /user:tscsa\admin Kambio.891 /p:yes""", creationflags=CREATE_NO_WINDOW)
+
+home_dir = Path.home()
+deletefiles = (f'{ home_dir }\\Textile Sourcing Company S.A.C\\TI-Soporte e Infraestructura - Controles\\Backup')
+extensions = ('.pdf','.PDF','.docx','.DOCX','.png','.jpg')
+TODAY = str(date.today())
+YESTERDAY = str(date.today() - timedelta(days = 1))
 
 
-network_share_available = os.path.isdir(network_share_path)
+#ID1_SRV001 #ID3_MICRO23 #ID4_OCSINVENTORY #ID5_PCRVILCA #ID8_SRV002 #ID9_PCFACT13
+#ID13_SVFACT12 #ID17_TSCDCP002 #ID20_SRVSIGE11 #ID22_SRVSIGE12 #ID23_SORARITZY
+ID2_MICRO22 = (r"\\172.16.88.10\bk_v\BK_MICRO22\*")
+ID6_SERVICIOS_TSCCH = (r"\\172.16.88.10\bk_v\BK_SERVICIOS_TSCCH\*")
+ID7_SRVWEB11 = (r"\\172.16.88.10\bk_v\BK_SRVWEB11\*")
+ID10_SVPLANILLA = (r"\\172.16.88.10\bk_v\BK_PLANILLA\*")
+ID11_SVPLANILLA_BBDD = (r"\\172.16.88.10\bk_ftp\sql\planilla\*")
+ID12_SVFACT11 = (r"\\172.16.88.10\bk_v\BK_SVFACT11\*")
+ID14_SVMYSQL = (r"\\172.16.88.10\bk_v\BK_SVMYSQL\*")
+ID15_SVMYSQL_BBDD = (r"\\172.16.88.10\bk_ftp\sql\mysql\*")
+ID16_TSCDCP001 = (r"\\172.16.88.10\bk_v\BK_TSCDCP001\*")
+ID18_SRVAFL = (r"\\172.16.88.10\bk_v\BK_SRVAFL\172.16.87.9\*")
+ID19_SOFYA = (r"\\172.16.88.10\bk_ftp\sql\sofya\*")
+ID21_SEGURIDAD = (r"\\172.16.88.10\bk_ftp\sql\sige\*")
+ID24_FISICO_ARC = (r"\\172.16.88.10\bk_ftp\ora\fis\*")
+ID25_FISICO_FULL = (r"\\172.16.88.10\bk_ftp\ora\fis\*")
+ID26_LOGICO_USYSTEX = (r"\\172.16.88.10\bk_ftp\ora\log\*")
+ID27_LOGICO_SYSTEXTILRPT = (r"\\172.16.88.10\bk_ftp\ora\log\*")
+ID28_SRV_SISCONT = (r"\\172.16.88.10\bk_v\BK_SRVSISCONT\172.16.87.5\*")
+ID29_SISCONT = (r"\\172.16.88.10\bk_ftp\sql\siscont\*")
+ID30_SERVICIOS_LIMA = (r"\\172.16.88.10\bk_v\BK_SERVICIOS_LIMA\172.16.47.5\*")
+ID31_DHCP = (r"\\172.16.88.10\bk_ftp\dhcp\*")
 
-if network_share_available:
-    log.info("Share already connected.")
-else:
-    log.info("Connecting to share")
+def crearconexion():
+    global conexion
+    direccion_servidor = 'SRVAFL'
+    nombre_bd = 'SRV_BACKUP'
+    nombre_usuario = 'RPA'
+    password = '123qweASD!"#'
+    try:
+        conexion = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' +
+                              direccion_servidor+';DATABASE='+nombre_bd+';UID='+nombre_usuario+';PWD=' + password)
+    except Exception as e:
+        print("Ocurrió un error al conectar a SQL Server: ", e)
 
-    mount_command = "net use " + SHARE_PATH + " "
-    os.system(mount_command)
-    network_share_available = os.path.isdir(network_share_path)
+def sqlvalidation(server,idsql,yesterday,today):
+    file_list=[]
+    for filename in glob.glob(server):
+        if ".vbm" not in filename:
+            file_list.append(filename)
+        else:
+            pass
 
-    if network_share_available:
-        log.fine("Connection success.")
+    if len(file_list) != 0:
+        latest_file = max(file_list, key=os.path.getctime)
+        lastdate = str(time.strftime("%Y-%m-%d",time.localtime(os.path.getmtime(latest_file))))
+
+        if lastdate == TODAY:
+            crearconexion()
+            try:
+                with conexion.cursor() as cursor:
+                    consulta = "use SRV_BACKUP update TBL_RECORDS set idstatus = '3', reg_user = 'RPA', reg_date = convert(date,?) where idserver = ? and bkp_date = convert(date,?)"
+                    cursor.execute(consulta, today, idsql, today)
+                    conexion.commit()
+            except Exception as e:
+                print(e)
+            finally:
+                conexion.close()
+
+        elif lastdate == YESTERDAY:
+            crearconexion()
+            try:
+                with conexion.cursor() as cursor:
+                    consulta = "use SRV_BACKUP update TBL_RECORDS set idstatus = '3', reg_user = 'RPA', reg_date = convert(date,?) where idserver = ? and bkp_date = convert(date,?)"
+                    cursor.execute(consulta, today, idsql, yesterday)
+                    conexion.commit()
+            except Exception as e:
+                print(e)
+            finally:
+                conexion.close()
+        else:
+            crearconexion()
+            try:
+                with conexion.cursor() as cursor:
+                    consulta = "use SRV_BACKUP update TBL_RECORDS set idstatus = '1', reg_user = 'RPA', reg_date = convert(date,?) where idserver = ? and bkp_date = convert(date,?)"
+                    cursor.execute(consulta, today, idsql, yesterday)
+                    conexion.commit()
+            except Exception as e:
+                print(e)
+            finally:
+                conexion.close()
     else:
-        raise Exception("Failed to find storage directory.")
+        pass
+
+if __name__ == "__main__":
+    try:
+        sqlvalidation(ID2_MICRO22,"2",YESTERDAY, TODAY)
+        sqlvalidation(ID6_SERVICIOS_TSCCH,"6",YESTERDAY, TODAY)
+        sqlvalidation(ID7_SRVWEB11,"7",YESTERDAY, TODAY)
+        sqlvalidation(ID10_SVPLANILLA,"10",YESTERDAY, TODAY)
+        sqlvalidation(ID11_SVPLANILLA_BBDD,"11",YESTERDAY, TODAY)
+        sqlvalidation(ID12_SVFACT11,"12",YESTERDAY, TODAY)
+        sqlvalidation(ID14_SVMYSQL,"14",YESTERDAY, TODAY)
+        sqlvalidation(ID15_SVMYSQL_BBDD,"15",YESTERDAY, TODAY)
+        sqlvalidation(ID16_TSCDCP001,"16",YESTERDAY, TODAY)
+        sqlvalidation(ID18_SRVAFL,"18",YESTERDAY, TODAY)
+        sqlvalidation(ID19_SOFYA,"19",YESTERDAY, TODAY)
+        sqlvalidation(ID21_SEGURIDAD,"21",YESTERDAY, TODAY)
+        sqlvalidation(ID24_FISICO_ARC,"24",YESTERDAY, TODAY)
+        sqlvalidation(ID25_FISICO_FULL,"25",YESTERDAY, TODAY)
+        sqlvalidation(ID26_LOGICO_USYSTEX,"26",YESTERDAY, TODAY)
+        sqlvalidation(ID27_LOGICO_SYSTEXTILRPT,"27",YESTERDAY, TODAY)
+        sqlvalidation(ID28_SRV_SISCONT,"28",YESTERDAY, TODAY)
+        sqlvalidation(ID29_SISCONT,"29",YESTERDAY, TODAY)
+        sqlvalidation(ID30_SERVICIOS_LIMA,"30",YESTERDAY, TODAY)
+        sqlvalidation(ID31_DHCP,"31",YESTERDAY, TODAY)
+
+        for x in os.listdir(deletefiles):
+            if os.path.isfile(x):
+                if not(x.endswith(extensions)):
+                    os.remove(str(deletefiles) + "\\" + str(x))
+
+        crearconexion()
+        with conexion.cursor() as cursor:
+            consulta = "use SRV_BACKUP update TBL_RECORDS set idstatus = 1, reg_user = 'RPA', reg_date = convert(date,?) where bkp_date != convert(date,?) and idstatus = 2"
+            cursor.execute(consulta,TODAY,TODAY)
+            conexion.commit()
+
+    except Exception as e:
+        print(e)
+
+    finally:
+            conexion.close()
